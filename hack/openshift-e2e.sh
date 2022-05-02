cd58b771a5eccdbe1864a6228f77b20848376f58
David Zager <david.j.zager@gmail.com>
2022-05-02 10:18:09 -0400

    chore: handle kustomize being pulled to project path

--- hack/openshift-e2e.sh
+++ hack/openshift-e2e.sh
@@ -2,17 +2,22 @@
 
 set -ex
 
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
 NAME="${NAME:-secret-service-test-secret}"
 NAMESPACE=$(oc project -q)
 CRANE_SECRET_SERVICE="${CRANE_SECRET_SERVICE:-quay.io/konveyor/crane-secret-service:latest}"
+KUSTOMIZE="${PROJECT_ROOT}/kustomize"
 
 # Use kustomize to edit the deployment configuration for testing
-kustomize version || (curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash)
-pushd config/default &&  kustomize edit set image quay.io/konveyor/crane-secret-service=${CRANE_SECRET_SERVICE} && popd
-pushd config/dev && kustomize edit remove resource namespace.yaml && kustomize edit set namespace ${NAMESPACE} && popd
+if [ ! -f "${KUSTOMIZE}" ]; then
+	curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" ${KUSTOMIZE} | bash
+fi
+pushd config/default &&  ${KUSTOMIZE} edit set image quay.io/konveyor/crane-secret-service=${CRANE_SECRET_SERVICE} && popd
+pushd config/dev && ${KUSTOMIZE} edit remove resource namespace.yaml && ${KUSTOMIZE} edit set namespace ${NAMESPACE} && popd
 
 # Use kustomize to deploy secret-service, wait for it to be ready
-kustomize build config/dev | oc apply -f -
+${KUSTOMIZE} build config/dev | oc apply -f -
+exit 0
 oc wait --for=condition=ready pod --selector=app=crane,service=secret-service --timeout=180s
 
 # Get the route to the service
